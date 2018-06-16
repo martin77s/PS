d4aa76ca55e82e25b3d7460c49d8bba199020a98
martin77s <maschvar@microsoft.com>
2018-06-16 22:38:29 +0300

    content updates

--- PSModules/AzStuff/AzStuff.psm1
+++ PSModules/AzStuff/AzStuff.psm1
@@ -38,9 +38,11 @@ function Test-azLogin {
         }
         $return = Get-AzureRmContext -ErrorAction Stop
 
-    } catch [System.Management.Automation.PSInvalidOperationException] {
+    }
+    catch [System.Management.Automation.PSInvalidOperationException] {
         $return = Login-AzureRmAccount
-    } catch {
+    }
+    catch {
         Write-Error $_.Exception.Message
     }
     [bool]$return
@@ -48,26 +50,75 @@ function Test-azLogin {
 
 
 function Get-azLocation {
-    Get-AzureRmLocation | 
-        Select-Object @{N='Zone';E={
+    Get-AzureRmLocation |
+        Select-Object @{N = 'Zone'; E = {
             $_.DisplayName -replace 'East|West|Southeast|South|North|Northeast|Central|\s|\d'
-        }}, DisplayName, Location, Providers |
-                Sort-Object -Property Zone, Location
+        }
+    }, DisplayName, Location, Providers |
+        Sort-Object -Property Zone, Location
 }
 
 
 function Get-azRandomName {
     param(
-        [string] $Prefix, 
+        [string] $Prefix,
         [int] $MaxLength = 24
     )
     $TotalLength = $MaxLength - ($Prefix.Length)
     if($TotalLength -gt 0) {
         $rnd = (1..$TotalLength | % {'abcdefghijklmnopqrstuvwxyz0123456789'.ToCharArray() | Get-Random}) -join ''
         "$Prefix$rnd"
-    } else {
+    }
+    else {
         throw 'Prefix exceeds maximum length'
     }
 }
 
 
+function Get-azVMSize {
+    param(
+        [string] $Location,
+        [stirng] $Filter,
+        [switch] $NoGrouping
+    )
+
+    if($Location) {
+        $locations = Get-AzureRmResourceProvider |
+            Where-Object { $_.ResourceTypes.ResourceTypeName -contains 'locations/vmSizes' } |
+            Select-Object -ExpandProperty Locations
+    }
+    else {
+        $locations = $Location
+    }
+
+    $sizes = $locations | ForEach-Object { $location = $_; Get-AzureRmVMSize -Location $_ } |
+        Select-Object Name, @{N = 'Location'; E = {$location}}
+
+    if($Filter) {
+        $sizes = $sizes | Where-Object { $_ -like $Filter }
+    }
+
+    if(-not $NoGrouping) {
+        $sizes | Group-Object -Property Name |
+            Select-Object Name, @{N = 'Locations'; E = {($_.Group | Select-Object -ExpandProperty Location) -join ', '}}
+    }
+    else {
+        $sizes
+    }
+}
+
+
+function Set-azVMAdminPassword {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $VMName,
+
+        [Parameter(Mandatory = $true)]
+        [PSCredential] $Credential
+    )
+    Set-AzureRmVMAccessExtension -Name MyVMAccessExt @PSBoundParameters
+}
