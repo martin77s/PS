eaeb556c319688addd1789cdc171c1bd73e563d0
Martin Schvartzman <martin77s@gmail.com>
2019-08-26 13:06:22 +0300

    content updates

---
+++ Scripts/Encrypt and Decrypt with CMS.ps1
@@ -0,0 +1,33 @@
+﻿# Get the script content as string:
+$scriptContent = Get-Content -Path C:\Temp\plainTextScript.ps1
+
+# Create the certificate:
+$cert = New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\CurrentUser\my `
+    -KeyUsage KeyEncipherment, DataEncipherment, KeyAgreement -Type DocumentEncryptionCert
+
+# Encrypt message:
+$scriptContent | Protect-CmsMessage -To $cert -OutFile C:\Temp\encryptedScript.txt
+
+# Verify the encrypted contents:
+notepad C:\Temp\encryptedScript.txt
+
+# Export the certificate:
+$certPassword = 'Password1' | ConvertTo-SecureString -AsPlainText -Force
+Export-PfxCertificate -Cert $cert -FilePath C:\Temp\CMS.pfx -Password $certPassword
+
+
+
+# *** ON THE NEXT MACHINE ***
+
+
+# Import the certificate:
+$certPassword = 'Password1' | ConvertTo-SecureString -AsPlainText -Force
+Import-PfxCertificate -FilePath C:\Temp\CMS.pfx -CertStoreLocation Cert:\CurrentUser\My -Password $certPassword
+
+
+# Decrypt message (The cert needs to be installed on the machine):
+$plainTextcode = Unprotect-CmsMessage -Path C:\Temp\encryptedScript.txt
+
+
+# Invoke the code:
+Invoke-Expression -Command $plainTextcode
---
+++ Scripts/Get-GmsaPassword.ps1
@@ -0,0 +1,14 @@
+﻿$svc = [ADSI] 'LDAP://cn=myGMSA,CN=Managed Service Accounts,DC=contoso,DC=coml'
+
+$svc.AuthenticationType = 'Sealing,Secure'
+$svc.RefreshCache('msDS-ManagedPassword')
+$svc.'msDS-ManagedPassword'.Value
+$gmsaBlob = $svc.'msDS-ManagedPassword'.Value
+
+[UInt16] $pwdOffset = [BitConverter]::ToUInt16($gmsaBlob[8..9], 0) # Note: this value must not be 0x0000
+[UInt16] $previousPwdOffset = [BitConverter]::ToUInt16($gmsaBlob[10..11], 0) # Note: if this value is 0x0000 then the account does not have any previous password
+
+$pwdEnd = $pwdOffset
+while (($gmsaBlob[$pwdEnd] -ne 0) -or ($gmsaBlob[$pwdEnd + 1] -ne 0)) { $pwdEnd += 2 }
+[byte[]] $gmsaPwd = $gmsaBlob[$pwdOffset..$pwdEnd]
+[BitConverter]::ToString($gmsaPwd)
